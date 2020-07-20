07a91ab92c988befe0f7f90c61fe9d1feb1e3dcf
Azim Momin <amomin@zynga.com>
2020-07-20 08:24:36 -0700

    Player movement and clamping position to screen.

---
+++ Prototype2/Assets/Scripts/Boundaries.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Renderer objRenderer;
+    [SerializeField] private bool boundX = false;
+    [SerializeField] private bool boundY = false;
+
+    private Vector2 screenBounds = Vector2.zero;
+    private Vector2 objectBounds = Vector2.zero;
+
+    private void Start()
+    {
+        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
+        objectBounds.x = objRenderer.bounds.size.x / 2f;
+        objectBounds.y = objRenderer.bounds.size.y / 2f;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 clampedPosition = transform.position;
+        if (boundX)
+            clampedPosition.x = Mathf.Clamp(clampedPosition.x, -screenBounds.x + objectBounds.x, screenBounds.x - objectBounds.x);
+
+        if (boundY)
+            clampedPosition.y = Mathf.Clamp(clampedPosition.y, -screenBounds.y + objectBounds.y, screenBounds.y - objectBounds.y);
+
+        transform.position = clampedPosition;
+    }
+}
---
+++ Prototype2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+
+    private float horizontalInput = 0f;
+    // The fraction of the screen on the left and right
+    // that the player is not allowed to move to.
+    private float deadZoneFraction = 10f;
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 playerScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        playerScreenPosition.x = Mathf.Clamp(
+            playerScreenPosition.x,
+            Screen.width / deadZoneFraction,
+            Screen.width - (Screen.width / deadZoneFraction)
+        );
+
+        transform.position = Camera.main.ScreenToWorldPoint(playerScreenPosition);
+    }
+}
